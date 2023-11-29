29c3cb31204af7fc09fa0c7cd38627b7d9711043
FenixSC2 <nathanbarth3141@gmail.com>
2023-11-29 17:18:14 -0500

    Fixed the bug
    The code runs again, currently working on implementing ASCII draw

--- lib/src/GameBoard.cpp
+++ lib/src/GameBoard.cpp
@@ -21,18 +21,8 @@ GameBoard::GameBoard(int bs, vector<Ship> s, bool placedRandomly) {
 
 // initialize the game board
 void GameBoard::initBoard() {
-    // reserve space on the board
-    board.reserve(boardSize);
-    for (int i = 0; i < boardSize; ++i) {
-        board[i].reserve(boardSize);
-    }
-
-    // fill the board with nullptrs
-    for (vector<Ship*> x : board) {
-        for (Ship* &y : x) {
-            y = nullptr;
-        }
-    }
+    // resize the board to the specified size and fill with nullptrs
+    board.resize(boardSize, vector<Ship*>(boardSize, nullptr));
 }
 
 void GameBoard::setSail() {
